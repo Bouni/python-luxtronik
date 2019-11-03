340298db6d01196383db260f1f8836124952c1b9
Bouni <bouni@owee.de>
2019-11-03 12:15:57 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="luxtronik",
-    version="0.2.4",
+    version="0.2.5",
     author="Bouni",
     author_email="bouni@owee.de",
     description="A luxtronik heatpump controller interface",
