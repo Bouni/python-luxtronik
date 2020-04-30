afea97676c5f88e8ae8b4f5ca07212ba3c5c7e80
bouni <bouni@owee.de>
2020-04-30 10:47:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A luxtronik heatpump controller interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bouni/luxtronik",
+    url="https://github.com/bouni/python-luxtronik",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
