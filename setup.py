de3b30690d66d9b01b47ed11f5be8c17fcbd4bcc
Bouni <bouni@owee.de>
2022-04-12 13:54:11 +0200

    add try except to catch response length mismatch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="luxtronik",
-    version="0.3.9",
+    version="0.3.10",
     author="Bouni",
     author_email="bouni@owee.de",
     description="A luxtronik heatpump controller interface",
