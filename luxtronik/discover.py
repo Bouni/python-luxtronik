01e4c4ad2a7790dd28de51222052691873f3596a
BenPru <32298537+BenPru@users.noreply.github.com>
2023-01-21 00:08:02 +0100

    Consts, port validation
    * LUXTRONIK_DISCOVERY_TIMEOUT const
    * LUXTRONIK_DISCOVERY_MAGIC_PACKET const
    * LUXTRONIK_DISCOVERY_RESPONSE_PREFIX const

--- luxtronik/discover.py
+++ luxtronik/discover.py
@@ -3,6 +3,9 @@ import logging
 import socket
 
 LUXTRONIK_DISCOVERY_PORTS = (4444, 47808)
+LUXTRONIK_DISCOVERY_TIMEOUT = 2
+LUXTRONIK_DISCOVERY_MAGIC_PACKET = "2000;111;1;\x00"
+LUXTRONIK_DISCOVERY_RESPONSE_PREFIX = "2500;111;"
 
 LOGGER = logging.getLogger("Luxtronik.Discover")
 
@@ -17,33 +20,35 @@ def discover() -> list[(str, int)]:
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         server.bind(("", magic_port))
-        server.settimeout(2)
+        server.settimeout(LUXTRONIK_DISCOVERY_TIMEOUT)
 
         # send AIT magic broadcast packet
-        data = "2000;111;1;\x00"
-        server.sendto(data.encode(), ("<broadcast>", magic_port))
-        LOGGER.debug("Sending broadcast request %s", data.encode())
+        server.sendto(LUXTRONIK_DISCOVERY_MAGIC_PACKET.encode(), ("<broadcast>", magic_port))
+        LOGGER.debug("Sending broadcast request %s", LUXTRONIK_DISCOVERY_MAGIC_PACKET.encode())
 
         while True:
             try:
                 res, con = server.recvfrom(1024)
                 res = res.decode("ascii", errors="ignore")
                 # if we receive what we just sent, continue
-                if res == data:
+                if res == LUXTRONIK_DISCOVERY_MAGIC_PACKET:
                     continue
                 ip_address = con[0]
                 # if the response starts with the magic nonsense
-                if res.startswith("2500;111;"):
+                if res.startswith(LUXTRONIK_DISCOVERY_RESPONSE_PREFIX):
                     res = res.split(";")
                     LOGGER.debug("Received answer from %s %s", ip_address, str(res))
                     try:
                         port = int(res[2])
+                        if port < 1 or port > 65535:
+                            port = None
                     except ValueError:
+                        port = None
+                    if port is None:
                         LOGGER.debug(
                             "Response did not contain a valid port number,"
                             "an old Luxtronic software version might be the reason."
                         )
-                        port = None
                     results.append((ip_address, port))
                 LOGGER.debug(
                     "Received answer, but with wrong magic bytes,"
