086752509cb2510b1cc9787aed7101d2d1a7122b
Bouni <bouni@owee.de>
2023-01-20 16:54:28 +0100

    Fix linter errors

--- luxtronik/discover.py
+++ luxtronik/discover.py
@@ -1,3 +1,4 @@
+"""Discovery funtion for Luxtronik2 heatpump controllers."""
 import logging
 import socket
 
@@ -6,17 +7,17 @@ LOGGER = logging.getLogger("Luxtronik.Discover")
 def discover():
     """Broadcast discovery for luxtronik heatpumps."""
 
-    for p in (4444, 47808):
-        LOGGER.debug(f"Send discovery packets to port {p}")
+    for magic_port in (4444, 47808):
+        LOGGER.debug("Send discovery packets to port %s", magic_port)
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        server.bind(("", p))
+        server.bind(("", magic_port))
         server.settimeout(2)
 
         # send AIT magic broadcast packet
         data = "2000;111;1;\x00"
-        server.sendto(data.encode(), ("<broadcast>", p))
-        LOGGER.debug(f'Sending broadcast request "{data.encode()}"')
+        server.sendto(data.encode(), ("<broadcast>", magic_port))
+        LOGGER.debug("Sending broadcast request %s", data.encode())
 
         while True:
             try:
@@ -25,25 +26,25 @@ def discover():
                 # if we receive what we just sent, continue
                 if res == data:
                     continue
-                ip = con[0]
+                ip_address = con[0]
                 # if the response starts with the magic nonsense
                 if res.startswith("2500;111;"):
                     res = res.split(";")
-                    LOGGER.debug(f'Received answer from {ip} "{res}"')
+                    LOGGER.debug("Received answer from %s:%d", ip_address, res)
                     try:
                         port = int(res[2])
                     except ValueError:
                         LOGGER.debug(
-                            "Response did not contain a valid port number, an old Luxtronic software version might be the reason."
+                            "Response did not contain a valid port number,"
+                            "an old Luxtronic software version might be the reason."
                         )
                         port = None
-                    return (ip, port)
-                # if not, continue
-                else:
-                    LOGGER.debug(
-                        f"Received answer, but with wrong magic bytes, from {ip} skip this one"
-                    )
-                    continue
+                    return (ip_address, port)
+                LOGGER.debug(
+                    "Received answer, but with wrong magic bytes,"
+                    "from %s skip this one", ip_address
+                )
+                continue
             # if the timeout triggers, go on an use the other broadcast port
             except socket.timeout:
                 break
