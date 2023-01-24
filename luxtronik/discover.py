a1e788ed1993f0a9734ab054c30cc26b319ccbce
BenPru <32298537+BenPru@users.noreply.github.com>
2023-01-25 00:19:34 +0100

    Fix wrong data types and flake warnings

--- luxtronik/discover.py
+++ luxtronik/discover.py
@@ -1,4 +1,4 @@
-"""Discovery function for Luxtronik heat pump controllers"""
+"""Discovery function for Luxtronik heat pump controllers."""
 import logging
 import socket
 
@@ -17,10 +17,10 @@ LUXTRONIK_DISCOVERY_RESPONSE_PREFIX = "2500;111;"
 LOGGER = logging.getLogger("Luxtronik.Discover")
 
 
-def discover() -> list[(str, int)]:
-    """Broadcast discovery for Luxtronik heat pumps"""
+def discover() -> list[tuple[str, int | None]]:
+    """Broadcast discovery for Luxtronik heat pumps."""
 
-    results: list[(str, int)] = []
+    results: list[tuple[str, int | None]] = []
 
     # pylint: disable=too-many-nested-blocks
     for port in LUXTRONIK_DISCOVERY_PORTS:
@@ -32,35 +32,37 @@ def discover() -> list[(str, int)]:
 
         # send AIT magic broadcast packet
         server.sendto(LUXTRONIK_DISCOVERY_MAGIC_PACKET.encode(), ("<broadcast>", port))
-        LOGGER.debug("Sending broadcast request %s", LUXTRONIK_DISCOVERY_MAGIC_PACKET.encode())
+        LOGGER.debug(
+            "Sending broadcast request %s", LUXTRONIK_DISCOVERY_MAGIC_PACKET.encode()
+        )
 
         while True:
             try:
-                res, con = server.recvfrom(1024)
-                res = res.decode("ascii", errors="ignore")
+                recv_bytes, con = server.recvfrom(1024)
+                res = recv_bytes.decode("ascii", errors="ignore")
                 # if we receive what we just sent, continue
                 if res == LUXTRONIK_DISCOVERY_MAGIC_PACKET:
                     continue
                 ip_address = con[0]
                 # if the response starts with the magic nonsense
                 if res.startswith(LUXTRONIK_DISCOVERY_RESPONSE_PREFIX):
-                    res = res.split(";")
-                    LOGGER.debug("Received response from %s %s", ip_address, str(res))
+                    res_list = res.split(";")
+                    LOGGER.debug(
+                        "Received response from %s %s", ip_address, str(res_list)
+                    )
                     try:
-                        port = int(res[2])
-                        if port < 1 or port > 65535:
-                            LOGGER.debug(
-                                    "Response contained an invalid port, ignoring"
-                                    )
-                            port = None
+                        res_port: int | None = int(res_list[2])
+                        if res_port is None or res_port < 1 or res_port > 65535:
+                            LOGGER.debug("Response contained an invalid port, ignoring")
+                            res_port = None
                     except ValueError:
-                        port = None
-                    if port is None:
+                        res_port = None
+                    if res_port is None:
                         LOGGER.debug(
                             "Response did not contain a valid port number,"
-                            "an old Luxtronic software version might be the reason."
+                            "an old Luxtronic software version might be the reason"
                         )
-                    results.append((ip_address, port))
+                    results.append((ip_address, res_port))
                 LOGGER.debug(
                     "Received response from %s, but with wrong content, skipping",
                     ip_address,
