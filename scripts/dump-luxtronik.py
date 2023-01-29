303aa86f12356b55c880519a823c795b07d7ecf3
BenPru <32298537+BenPru@users.noreply.github.com>
2023-01-29 23:33:48 +0100

    Line breaks for linter

--- scripts/dump-luxtronik.py
+++ scripts/dump-luxtronik.py
@@ -9,40 +9,46 @@ import argparse
 from luxtronik import Luxtronik
 
 parser = argparse.ArgumentParser(
-        description="Dumps all values from Luxtronik controller"
-    )
+    description="Dumps all values from Luxtronik controller"
+)
+parser.add_argument("ip", help="IP address of Luxtronik controller to connect to")
 parser.add_argument(
-        "ip",
-        help="IP address of Luxtronik controller to connect to"
-    )
-parser.add_argument(
-        "port",
-        nargs="?",
-        type=int,
-        default=8889,
-        help="Port to use to connect to Luxtronik controller"
-    )
+    "port",
+    nargs="?",
+    type=int,
+    default=8889,
+    help="Port to use to connect to Luxtronik controller",
+)
 args = parser.parse_args()
 
 client = Luxtronik(args.ip, args.port)
 
-print("="*80)
+print("=" * 80)
 print(f"{' Parameter ': ^80}")
-print("="*80)
+print("=" * 80)
 
 for number, param in client.parameters.parameters.items():
-    print(f"Number: {number:<5} Name: {param.name:<60} Type: {param.__class__.__name__:<20} Value: {param.value}")
+    print(
+        f"Number: {number:<5} Name: {param.name:<60} "
+        + f"Type: {param.__class__.__name__:<20} Value: {param.value}"
+    )
 
-print("="*80)
+print("=" * 80)
 print(f"{' Calculations ': ^80}")
-print("="*80)
+print("=" * 80)
 
 for number, calc in client.calculations.calculations.items():
-    print(f"Number: {number:<5} Name: {calc.name:<60} Type: {calc.__class__.__name__:<20} Value: {calc.value}")
+    print(
+        f"Number: {number:<5} Name: {calc.name:<60} "
+        + f"Type: {calc.__class__.__name__:<20} Value: {calc.value}"
+    )
 
-print("="*80)
+print("=" * 80)
 print(f"{' Visibilities ': ^80}")
-print("="*80)
+print("=" * 80)
 
 for number, visi in client.visibilities.visibilities.items():
-    print(f"Number: {number:<5} Name: {visi.name:<60} Type: {visi.__class__.__name__:<20} Value: {visi.value}")
+    print(
+        f"Number: {number:<5} Name: {visi.name:<60} "
+        + f"Type: {visi.__class__.__name__:<20} Value: {visi.value}"
+    )
