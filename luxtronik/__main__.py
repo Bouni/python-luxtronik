40cd459fa2407b8dc6ff245a107b115cb940bdc0
Bouni <bouni@owee.de>
2023-01-30 14:51:05 +0100

    moved and renamed dump script
    wrapped dump script in function
    
    changes to make script dump import work
    
    Neat solution for CLI and scripts
    
    making pylint happy :smirk:
    
    Update comment as suggested by @kbabioch
    
    Co-authored-by: Karol Babioch <karol@babioch.de>
    
    Update luxtronik/__main__.py as suggested by @kbabioch
    
    Co-authored-by: Karol Babioch <karol@babioch.de>
    
    Update comment as suggested by @kbabioch
    
    Co-authored-by: Karol Babioch <karol@babioch.de>
    
    Update help text as suggested by @kbabioch
    
    Co-authored-by: Karol Babioch <karol@babioch.de>
    
    Update comment as suggested by @kbabioch
    
    Co-authored-by: Karol Babioch <karol@babioch.de>
    
    Update help text as suggested by @kbabioch
    
    Co-authored-by: Karol Babioch <karol@babioch.de>
    
    Avoid hacky calling of functions

---
+++ luxtronik/__main__.py
@@ -0,0 +1,45 @@
+"""Luxtronik CLI."""
+import argparse
+import sys
+from luxtronik.discover import discover as _discover
+from luxtronik.scripts.dump import dump  # pylint: disable=unused-import # noqa: F401
+
+
+def discover():
+    """Output available heat pump controllers nicely."""
+    results = _discover()
+    print(f"{len(results)} heatpump(s) reported back")
+    for count, result in enumerate(results):
+        print(f"Heat pump #{count} -> IP address: {result[0]} port: {result[1]}")
+
+
+def test():
+    print("Yo")
+
+
+def main() -> int:
+    """Entry point to parse CLI arguments."""
+    parser = argparse.ArgumentParser(
+        description="CLI for Luxtronik controllers",
+        usage="""luxtronik <command> [<args>]
+        The supported commands are:
+        dump       Dump all available data from the Luxtronik controller 
+        discover   Discover Luxtronik controllers on the network (via magic packet) and output results
+        """,
+    )
+    parser.add_argument("command", help="Subcommand to run")
+    # parse_args defaults to [1:] for args, but you need to
+    # exclude the rest of the args too, or validation will fail
+    args = parser.parse_args(sys.argv[1:2])
+    commands = {
+        "dump": dump,
+        "discover": discover,
+    }
+    if args.command not in commands:
+        print("Unrecognized command")
+        parser.print_help()
+        sys.exit(1)
+    # pop command, otherwise the argparser within the called script will fail
+    sys.argv.pop(1)
+    # call the corresponding command
+    commands[args.command]()
