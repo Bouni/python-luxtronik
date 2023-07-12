382083f51103703097393a93ac1c9ccd310b3607
Bouni <bouni@owee.de>
2023-07-12 14:20:28 +0200

    Replace port number with constant

--- luxtronik/scripts/dump_changes.py
+++ luxtronik/scripts/dump_changes.py
@@ -9,6 +9,7 @@ import time
 import argparse
 
 from luxtronik import Luxtronik
+from luxtronik.constants import LUXTRONIK_DEFAULT_PORT
 
 
 def dump_changes():
@@ -22,7 +23,7 @@ def dump_changes():
         "port",
         nargs="?",
         type=int,
-        default=8889,
+        default=LUXTRONIK_DEFAULT_PORT,
         help="Port to use to connect to Luxtronik controller",
     )
     args = parser.parse_args()
