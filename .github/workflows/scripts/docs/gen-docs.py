dde66b6c4a311e9f51d6883c06347068801e1fb5
Bouni <bouni@owee.de>
2024-12-16 09:02:09 +0100

    Cleanup 🧹

--- .github/workflows/scripts/docs/gen-docs.py
+++ .github/workflows/scripts/docs/gen-docs.py
@@ -10,17 +10,14 @@ from luxtronik.visibilities import Visibilities
 
 logging.basicConfig(level=logging.INFO)
 
-logger = logging.getLogger("doc-gen")
+logger = logging.getLogger("docs generator")
 
 
 BASEPATH = Path(__file__).resolve().parent
 
 
-# for no, parameter in p._data.items():
-#     print(no, parameter.name)
-
-
 def gather_data() -> dict:
+    logger.info("gather docs data")
     p = Parameters()
     c = Calculations()
     v = Visibilities()
@@ -36,19 +33,6 @@ def gather_data() -> dict:
     return data
 
 
-# def parse_group(group: Literal["calculations", "parameters", "visibilities"]) -> list[dict]:
-#     logger.info("parse group '%s'", group)
-#     data = []
-#     with open(BASEPATH.parents[3] / f"luxtronik/{group}.py") as f:
-#         raw = f.read()
-#         regex = re.compile(r"(?P<number>\d+):\s+(?P<type>[^(]+)\(\"(?P<name>[^\"]+)\"\)")
-#         results = [m.groupdict() for m in regex.finditer(raw)]
-#         logger.info("found %d entries", len(results))
-#         for r in results:
-#             data.append({"number": r["number"], "type": r["type"], "name": r["name"]})
-#     return data
-#
-#
 def render_docs():
     logger.info("render docs")
     env = Environment(loader=FileSystemLoader(str(BASEPATH / "templates")), autoescape=select_autoescape())
@@ -58,8 +42,6 @@ def render_docs():
     with open(BASEPATH.parents[3] / "docs/index.html", "w", encoding="UTF-8") as f:
         f.write(template.render(data=group_data))
 
-
-#
 
 if __name__ == "__main__":
     render_docs()
