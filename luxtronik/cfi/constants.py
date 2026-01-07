47395d333195625893261cbc0f0ad3f8ef01bd54
Guzz-T <Guzz-T@users.noreply.github.com>
2026-01-07 21:12:23 +0100

    Use `CONST: Final` consistently throughout the module

--- luxtronik/cfi/constants.py
+++ luxtronik/cfi/constants.py
@@ -1,23 +1,25 @@
 """Constants used throughout the Luxtronik config interface (CFI) module."""
 
+from typing import Final
+
 # Default port to be used to connect to Luxtronik controller.
-LUXTRONIK_DEFAULT_PORT = 8889
+LUXTRONIK_DEFAULT_PORT: Final = 8889
 
-LUXTRONIK_PARAMETERS_WRITE = 3002
-LUXTRONIK_PARAMETERS_READ = 3003
-LUXTRONIK_CALCULATIONS_READ = 3004
-LUXTRONIK_VISIBILITIES_READ = 3005
+LUXTRONIK_PARAMETERS_WRITE: Final = 3002
+LUXTRONIK_PARAMETERS_READ: Final = 3003
+LUXTRONIK_CALCULATIONS_READ: Final = 3004
+LUXTRONIK_VISIBILITIES_READ: Final = 3005
 
-LUXTRONIK_SOCKET_READ_SIZE_PEEK = 16
+LUXTRONIK_SOCKET_READ_SIZE_PEEK: Final = 16
 
-LUXTRONIK_SOCKET_READ_SIZE_INTEGER = 4
-LUXTRONIK_SOCKET_READ_SIZE_CHAR = 1
+LUXTRONIK_SOCKET_READ_SIZE_INTEGER: Final = 4
+LUXTRONIK_SOCKET_READ_SIZE_CHAR: Final = 1
 
 # Identifier of calculation data-vectors and partial name for unknown calculation fields
-CALCULATIONS_FIELD_NAME = "calculation"
+CALCULATIONS_FIELD_NAME: Final = "calculation"
 
 # Identifier of parameter data-vectors and partial name for unknown parameter fields
-PARAMETERS_FIELD_NAME = "parameter"
+PARAMETERS_FIELD_NAME: Final = "parameter"
 
 # Identifier of visibilities data-vectors and partial name for unknown visibility fields
-VISIBILITIES_FIELD_NAME = "visibility"
+VISIBILITIES_FIELD_NAME: Final = "visibility"
