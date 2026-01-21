055e460a4f7b45cd1916b123109e2a1aa6bb0d7e
Guzz-T <Guzz-T@users.noreply.github.com>
2026-01-21 23:20:51 +0100

    Extend the pytest for LuxtronikDefFieldPair

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -3,6 +3,7 @@ from luxtronik.collections import (
     get_data_arr,
     check_data,
     integrate_data,
+    LuxtronikDefFieldPair,
     LuxtronikFieldsDictionary,
 )
 from luxtronik.definitions import LuxtronikDefinition, LuxtronikDefinitionsDictionary
@@ -19,9 +20,22 @@ from luxtronik.shi.constants import LUXTRONIK_VALUE_FUNCTION_NOT_AVAILABLE
 
 class TestDefinitionFieldPair:
 
+    def test_init(self):
+        definition = LuxtronikDefinition.unknown(2, 'Foo', 30)
+        field = definition.create_field()
+        pair = LuxtronikDefFieldPair(definition, field)
+
+        assert pair.definition is definition
+        assert pair.field is field
+        d, f = pair
+        assert d is definition
+        assert f is field
+
     def test_data_arr(self):
         definition = LuxtronikDefinition.unknown(2, 'Foo', 30)
         field = definition.create_field()
+        pair = LuxtronikDefFieldPair(definition, field)
+
         field.concatenate_multiple_data_chunks = False
 
         # get from value
@@ -29,6 +43,7 @@ class TestDefinitionFieldPair:
         field.raw = 5
         arr = get_data_arr(definition, field)
         assert arr == [5]
+        assert arr == pair.get_data_arr()
         assert check_data(definition, field)
 
         # get from array
@@ -36,6 +51,7 @@ class TestDefinitionFieldPair:
         field.raw = [7, 3]
         arr = get_data_arr(definition, field)
         assert arr == [7, 3]
+        assert arr == pair.get_data_arr()
         assert check_data(definition, field)
 
         # too much data
@@ -43,6 +59,7 @@ class TestDefinitionFieldPair:
         field.raw = [4, 8, 1]
         arr = get_data_arr(definition, field)
         assert arr is None
+        assert arr == pair.get_data_arr()
         assert not check_data(definition, field)
 
         # insufficient data
@@ -50,6 +67,7 @@ class TestDefinitionFieldPair:
         field.raw = [9]
         arr = get_data_arr(definition, field)
         assert arr is None
+        assert arr == pair.get_data_arr()
         assert not check_data(definition, field)
 
         field.concatenate_multiple_data_chunks = True
@@ -59,6 +77,7 @@ class TestDefinitionFieldPair:
         field.raw = 0x0007_0003
         arr = get_data_arr(definition, field)
         assert arr == [7, 3]
+        assert arr == pair.get_data_arr()
         assert check_data(definition, field)
 
         # too much data
@@ -66,6 +85,7 @@ class TestDefinitionFieldPair:
         field.raw = 0x0004_0008_0001
         arr = get_data_arr(definition, field)
         assert arr == [8, 1]
+        assert arr == pair.get_data_arr()
         assert check_data(definition, field)
 
         # insufficient data
@@ -73,35 +93,37 @@ class TestDefinitionFieldPair:
         field.raw = 0x0009
         arr = get_data_arr(definition, field)
         assert arr == [0, 9]
+        assert arr == pair.get_data_arr()
         assert check_data(definition, field)
 
     def test_integrate(self):
         definition = LuxtronikDefinition.unknown(2, 'Foo', 30)
         field = definition.create_field()
-        field.concatenate_multiple_data_chunks = False
-
+        pair = LuxtronikDefFieldPair(definition, field)
         data = [1, LUXTRONIK_VALUE_FUNCTION_NOT_AVAILABLE, 3, 4, 5, 6, 7]
+
+        field.concatenate_multiple_data_chunks = False
 
         # set array
         definition._count = 2
         integrate_data(definition, field, data)
         assert field.raw == [3, 4]
-        integrate_data(definition, field, data, 4)
+        pair.integrate_data(data, 4)
         assert field.raw == [5, 6]
         integrate_data(definition, field, data, 7)
         assert field.raw is None
-        integrate_data(definition, field, data, 0)
+        pair.integrate_data(data, 0)
         assert field.raw is None
 
         # set value
         definition._count = 1
         integrate_data(definition, field, data)
         assert field.raw == 3
-        integrate_data(definition, field, data, 5)
+        pair.integrate_data(data, 5)
         assert field.raw == 6
         integrate_data(definition, field, data, 9)
         assert field.raw is None
-        integrate_data(definition, field, data, 1)
+        pair.integrate_data(data, 1)
         assert field.raw is None
 
         field.concatenate_multiple_data_chunks = True
@@ -110,22 +132,22 @@ class TestDefinitionFieldPair:
         definition._count = 2
         integrate_data(definition, field, data)
         assert field.raw == 0x0003_0004
-        integrate_data(definition, field, data, 4)
+        pair.integrate_data(data, 4)
         assert field.raw == 0x0005_0006
         integrate_data(definition, field, data, 7)
         assert field.raw is None
-        integrate_data(definition, field, data, 0)
+        pair.integrate_data(data, 0)
         assert field.raw is None
 
         # set value
         definition._count = 1
         integrate_data(definition, field, data)
         assert field.raw == 0x0003
-        integrate_data(definition, field, data, 5)
+        pair.integrate_data(data, 5)
         assert field.raw == 0x0006
         integrate_data(definition, field, data, 9)
         assert field.raw is None
-        integrate_data(definition, field, data, 1)
+        pair.integrate_data(data, 1)
         assert field.raw is None
 
 
